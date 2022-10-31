301081b235f7b21d07399bd91348c1a5ec92c6cc
Oluwatosin Oduwole <david2tosin@gmail.com>
2022-10-31 22:14:26 +0100

    Added Error Page and implemented React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,16 @@
 import "./index.css";
-import profile from "./assets/profile__img.png";
-import button1 from "./assets/_Avatar share button1.png";
-import slack from "./assets/slack.png"
-import github from "./assets/icon.png"
-import zuri from "./assets/vector.png"
-import globe from "./assets/I4G.png"
+import Home from "./Home";
+import ErrorPage from "./ErrorPage";
+import { Routes, Route, Link } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="app">
-      <main className="container">
-        <section className="profile-container">
-          <img id="profile__img" src={profile} alt="" />
-          <p id="twitter">David Tosin</p>
-          <p id="slack" className="slack">
-            Oduwole Oluwatosin David
-          </p>
-          <img className="share-button" src={button1} alt="" />
-        </section>
-        <section className="links">
-          <a href="https://twitter.com/David2Tosin">Twitter Link</a>
-          <a id="btn__zuri" href="https://training.zuri.team/">
-            Zuri Team
-          </a>
-          <a id="books" href="http://books.zuri.team/">
-            Zuri Books
-          </a>
-          <a
-            id="book__python"
-            href="https://books.zuri.team/python-for-beginners?ref_id=OduwoleOluwatosinDavid"
-          >
-            Python Books
-          </a>
-          <a id="pitch" href="https://background.zuri.team/">
-            Background Check for Coders
-          </a>
-          <a id="book__design" href="https://books.zuri.team/design-rules">
-            Design Books
-          </a>
-        </section>
-      </main>
-      <footer>
-        <div className="footer-one">
-          <div className="footer-one-logo">
-            <img className="" src={slack} alt="" />
-            <img  src={github} alt="" />
-          </div>
-        </div>
-        <div className="footer-two">
-          <div className="footer-two-logo">
-            <img src={zuri} alt="" />
-            <p className="hng">HNG Internship 9 Frontend Task</p>
-            <img src={globe} alt="" />
-          </div>
-        </div>
-      </footer>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div className="error">
+      <p className="l1">404 error</p>
+      <p className="l2">We can’t find that page</p>
+      <p className="l3">
+        Sorry, the page you are looking for doesn't exist or has been moved.
+      </p>
+      <Link className="l4" to="/">
+        Take me home
+      </Link>
+      <Link className="l5" to="/">
+        Go back
+      </Link>
+    </div>
+  );
+};
+export default ErrorPage;
